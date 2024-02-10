272032b0cf2d80c4bbb441524341b2ff4da579be
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-02-10 11:11:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+// Create an object called student with a property called name
+const student = {
+  name: "John"
+};
+
+// Add a property to the Object prototype called getKeys() that returns an array of all the keys in the object
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Test the getKeys() method
+console.log(student.getKeys()); // Output: ['name']
